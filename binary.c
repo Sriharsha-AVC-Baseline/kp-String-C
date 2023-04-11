64ae8ef7c2851798d3fb2fd1d219808355d71121
Sriharsha-AVC-Baseline <83340788+Sriharsha-AVC-Baseline@users.noreply.github.com>
2023-04-11 17:23:28 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<String.h>
+
+// Binary To Decimal Converter
+int binary(char *bnry,int length)
+{
+	int decimal=0;
+	int two=1,i;
+	for(i=length-1;i>=0;i--)
+	{
+		if(bnry[i]=='1')
+		{
+			decimal = decimal + two;
+			two=two*2;
+		}
+		else if(bnry[i]=='0')
+		{
+			two=two*2;
+		}
+	}
+	return decimal;
+}
+int main()
+{
+	printf("Enter the Binary value : ");
+	char str[100];
+	fgets(str,100,stdin);
+	int len = strlen(str);
+	printf("%d is the Decimal Value\n",binary(str,len));
+}
