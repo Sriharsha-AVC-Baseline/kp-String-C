ecb009c03a4b32fafe7c3d24e5d7de255090dc9e
Sriharsha-AVC-Baseline <83340788+Sriharsha-AVC-Baseline@users.noreply.github.com>
2023-04-11 17:27:11 +0530

    Add files via upload

---
+++ Camel_to_snake.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<String.h>
+#include<stdlib.h>
+
+void camel_to_snake(char* str)
+{
+    int length=strlen(str);
+    int i=0,j=0;
+    char nstr[200];
+    if(str[0]<96)
+    {
+        str[0]=str[0]+32;
+    }
+    for(i=0;i<length-1;i++)
+    {
+        if(str[i]<96)
+        {
+            nstr[j]='_';
+            j++;
+            str[i]=str[i]+32;
+        }
+        nstr[j]=str[i];
+        j++;
+    }
+    printf("%s",nstr);
+}
+
+int main()
+{
+    char str[200];
+    printf("Enter the CamelCase String\n");
+    fgets(str,100,stdin);
+    camel_to_snake(str);
+}
