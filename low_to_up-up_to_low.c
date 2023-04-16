2ef16c2937904c1959042be88e94fd8ecacf432b
Sriharsha-AVC-Baseline <83340788+Sriharsha-AVC-Baseline@users.noreply.github.com>
2023-04-16 09:37:33 +0530

    Add files via upload

---
+++ low_to_up-up_to_low.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<String.h>
+#include<stdlib.h>
+
+void viceverse(char* str)
+{
+    int length=strlen(str);
+    int i=0,j=0;
+    for(i=0;i<length-1;i++)
+    {
+       if(str[i]>64&&str[i]<96)
+       {
+        str[i]=str[i]+32;
+       }
+       else if(str[i]>=96)
+       {
+        str[i]=str[i]-32;
+       }
+    }
+    printf("%s",str);
+}
+
+int main()
+{
+    char str[200];
+    printf("Enter the String\n");
+    fgets(str,100,stdin);
+    viceverse(str);
+}
